b6ef6da0a2327daed432d9139a1927e8a35f9aab
abel pinheiro de figueiredo <abelpinheiro95@gmail.com>
2025-04-20 14:00:55 -0300

    Add player role in default system roles

--- src/KnowledgeQuiz.Api.Infrastructure/Migrations/20250420043455_AddPlayerSystemRole.Designer.cs
+++ src/KnowledgeQuiz.Api.Infrastructure/Migrations/20250420043455_AddPlayerSystemRole.Designer.cs
@@ -4,6 +4,7 @@ using KnowledgeQuiz.Api.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KnowledgeQuiz.Api.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250420043455_AddPlayerSystemRole")]
+    partial class AddPlayerSystemRole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
