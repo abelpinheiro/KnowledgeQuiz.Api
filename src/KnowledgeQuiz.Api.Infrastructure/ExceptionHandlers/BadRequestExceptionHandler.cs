d293c61853f7f387212a4e1030b040735e132dbe
abel pinheiro de figueiredo <abelpinheiro95@gmail.com>
2025-06-20 10:53:10 -0300

    add bad request exception handler

---
+++ src/KnowledgeQuiz.Api.Infrastructure/ExceptionHandlers/BadRequestExceptionHandler.cs
@@ -0,0 +1,48 @@
+﻿using KnowledgeQuiz.Api.Application.DTOs;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace KnowledgeQuiz.Api.Infrastructure.ExceptionHandlers;
+
+internal sealed class BadRequestExceptionHandler : IExceptionHandler
+{
+    private readonly ILogger<BadRequestExceptionHandler> _logger;
+
+    public BadRequestExceptionHandler(ILogger<BadRequestExceptionHandler> logger)
+    {
+        _logger = logger;
+    }
+    
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        if (exception is not BadHttpRequestException badRequestException)
+        {
+            return false;
+        }
+
+        _logger.LogError(
+            badRequestException,
+            "Exception occurred: {Message}",
+            badRequestException.Message);
+        
+        var apiResponse = ApiResponse<string>.Fail("Bad request test.");
+        await httpContext.Response.WriteAsJsonAsync(apiResponse, cancellationToken);
+        /*
+        var problemDetails = new ProblemDetails
+        {
+            Status = StatusCodes.Status400BadRequest,
+            Title = "Bad Request",
+            Detail = badRequestException.Message
+        };
+        
+        
+        httpContext.Response.StatusCode = problemDetails.Status.Value;
+
+        await httpContext.Response
+            .WriteAsJsonAsync(problemDetails, cancellationToken);*/
+
+        return true;
+    }
+}
